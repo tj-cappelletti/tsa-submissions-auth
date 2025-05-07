6ae65545bffd147c8737c8be8c4ceaedc19fa1fd
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-05-06 21:45:09 -0400

    Created status controller

---
+++ src/Tsa.Submissions.Auth.WebApi/Models/ServiceStatusModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Tsa.Submissions.Auth.WebApi.Models;
+
+public class ServicesStatusModel
+{
+    public bool IsHealthy => EvaluateIsHealthy();
+
+    private bool EvaluateIsHealthy()
+    {
+        var propertyInfos = GetType().GetProperties().Where(_ => _.CanWrite && _.PropertyType == typeof(bool));
+
+        var isAliveList = propertyInfos.Select(propertyInfo => (bool)propertyInfo.GetValue(this)!).ToList();
+
+        return isAliveList.All(isAlive => isAlive);
+    }
+}
---
+++ src/Tsa.Submissions.Auth.WebApi/Services/IPingableService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tsa.Submissions.Auth.WebApi.Services;
+
+public interface IPingableService
+{
+    string ServiceName { get; }
+
+    Task<bool> PingAsync(CancellationToken cancellationToken = default);
+}
